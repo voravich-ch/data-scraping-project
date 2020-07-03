f3c37cc05014ee911c1104b1b31aa929e8afa0e0
jimmy <j_crazy_j@hotmail.com>
2020-07-03 14:24:18 +0700

    Initial commit

---
+++ final_app.py
@@ -0,0 +1,61 @@
+import bs4
+import requests
+import pymongo
+import os
+import json
+import datetime
+
+def connect_to_database():
+    # ใช้ os.environ แทน ไม่ควร hardcode อะไรที่เป็น credential
+    # MONGODB_URI = os.environ['MONGODB_URI']
+    client = pymongo.MongoClient(MONGODB_URI, retryWrites = False)
+    db = client['heroku_0bjqvfwj']
+    collection_name = 'news_data'
+    if collection_name not in db.list_collection_names():
+        db.create_collection(collection_name)
+    collection = db[collection_name]
+    return collection
+
+
+def scrape_data():
+    response = requests.get('https://www.thairath.co.th/news/royal')
+    html_page = bs4.BeautifulSoup(response.content, 'html.parser')
+    data = html_page.find(id = '__NEXT_DATA__')
+    json_file = json.loads(str(data)[51:-9])
+    target_news = json_file['props']['initialState']['common']['data']['items']['lastestNews']
+    
+    document = []
+
+    for news in target_news:
+        content = bs4.BeautifulSoup(requests.get(news['canonical']).content, 'html.parser').select('div.css-n5piny.evs3ejl1 p')
+        desc = ''
+        for p in content:
+            desc = desc + p.text + '\n'
+        dict = {
+        'title': news['title'],
+        'publish_date': datetime.datetime.strptime(news['publishTime'][0:10] + ' ' + news['publishTime'][11:19], '%Y-%m-%d %H:%M:%S'),
+        'desc': desc,
+        'tags': news['tags'],
+        'cover_img': news['image'],
+        'news_url': news['canonical'],
+        'category': news['topic'],
+                }
+        document.append(dict)
+
+    return document
+
+
+def insert_to_database(collection, document):
+    collection.insert_one(document)
+    print(document)
+    print('Document inserted!')
+
+
+def main():
+    collection = connect_to_database()
+    document = scrape_data()
+    insert_to_database(collection, document)
+
+
+if __name__ == "__main__":
+    scrape_data()
